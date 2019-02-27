74b62bcb80f31397abd10700a7e61816e8017b36
卫浩波 <32203359+hubertway@users.noreply.github.com>
2019-02-27 19:22:58 +0800

    不能暴力法，得归纳出表达式。

--- PAT-Basic Level/1060 爱丁顿数 (25).cpp
+++ PAT-Basic Level/1060 爱丁顿数 (25).cpp
@@ -1,23 +1,22 @@
+//从下标1开始存储n天的公里数在数组a中，对n个数据从大到小排序，i表示了骑车的天数,那么满足a[i] > i的最大值即为所求。
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+bool cmp(int a, int b) {
+	return a > b;
+}
 int main() {
 	int N;
 	scanf("%d", &N);
-	vector<int>v(N);
-	for (int i = 0; i < N; i++) scanf("%d", &v[i]);
-
-	int day, mile, maxE=0; //天数、英里数、最大整数
-	for (int i = 0; i <= N; i++) {
-		day = i; mile = 0;
-		for (int j = 0; j < N; j++) {
-			if (v[j] > day) mile++;
-		}
-		if (day > mile) break;
-		else  {
-			if (maxE < day) maxE = day;
-		}
+	vector<int>v(N+1);
+	for (int i = 1; i <= N; i++) scanf("%d", &v[i]);
+	sort(v.begin() + 1, v.end(), cmp);
+	int day=1, maxE = 0; 
+	while (maxE <= N && v[day] > day) {
+		maxE++;
+		day++;
 	}
 	printf("%d", maxE);
 
