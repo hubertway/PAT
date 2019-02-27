c584f8ca97ad64c092fddc0b16f420904c62e51d
卫浩波 <32203359+hubertway@users.noreply.github.com>
2019-02-27 17:55:42 +0800

    需要把A和B变成同样的长度（通过吧较短的填零的方式）再进行加密得到答案C
    这题目题意理解有障碍:
    根据他人ac答案可推测，这里加密的既不是A，也不是B，
    而是需要把A和B变成同样的长度（通过吧较短的填零的方式）再进行加密得到答案C
    这个C通过B解密能得到A，通过A解密能得到B

--- PAT-Basic Level/1048. 数字加密(20).cpp
+++ PAT-Basic Level/1048. 数字加密(20).cpp
@@ -6,18 +6,36 @@ char h[13] = { '0','1','2','3','4','5','6','7','8','9','J','Q','K' };
 int main() {
 	string A, B;
 	cin >> A >> B;
-	int i = B.size() - 1;
-	int j = A.size() - 1;
+	int i = A.size() - 1;
+	int j = B.size() - 1;
+
+	if (i > j) {
+		int l = i - j;
+		string x;
+		for (int a = 0; a < l; a++) x += '0';
+		B = x + B;
+	}
+	if (i < j) {
+		int l = j - i;
+		string x;
+		for (int a = 0; a < l; a++) x += '0';
+		A = x + A;
+		i = j;
+	}
 	int t;
-	while ((i >= 0) && (j >= 0)) {
-		t = ((B[i] - '0') + (A[j] - '0')) % 13;
-		B[i] = h[t];
-		i--; j--;
-		if (!((i >= 0) && (j >= 0))) break;
-		t = B[i] - A[j] ;
-		if (t < 0) t += 10;
-		B[i] = h[t];
-		i--; j--;
+	int d = 1;
+	while (i >= 0) {
+		if (d % 2 != 0) { //奇数位
+			t = ((B[i] - '0') + (A[i] - '0')) % 13;
+			B[i] = h[t];
+		}
+		else {
+			t = B[i] - A[i];
+			if (t < 0) t += 10;
+			B[i] = h[t];
+		}
+		d++;
+		i--;
 	}
 	cout << B;
 
